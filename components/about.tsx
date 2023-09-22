80bde3a74ab74e07680f5725eb75a62435beb36a
candran054 <candran054@gmail.com>
2023-09-22 17:30:36 +0700

    add about-section

---
+++ components/about.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section>
+      <h2 className='text-3xl'>About me</h2>
+      <p>Graduated from Institut Teknologi & Bisnis Stikom Bali with{" "}
+        <span className='font-medium'>Software Engineer</span> degree.
+        I learned the basic programming with <span className='font-medium'>Borland C++</span> there and was taught
+        <span className='font-medium'> Object Oriented Programming (OOP)</span>.
+        If im being honest i did not enjoy it at all to the point that i feel like im not cut out to be a Programmer. Until i learn{" "}
+        <span>Html & CSS</span>, creating layout, adding image, navbar and changing the color of an element when you hover at the element was
+        something that i really enjoy. Then i start learning <span>Javascript</span> to add even more complexity when developing a website. After
+        graduating from college i decide i want to become a <span>Frontend Developer</span> and start improving my skill with <span>React</span>.
+        I Struggle so much with React but i managed to become good at it, not necessarily a master at <span>React</span> but i improved a lot.
+        Then i found <span>NextJS</span> and in my opinion making my job as <span>Frontend Developer</span> much easier, i found it to be better than
+        vannila React.
+      </p>
+    </section>
+  )
+}
